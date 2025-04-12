709d05cad8cdced7c0c7fdeeb045ad42b917c6b0
YeonhaPark <yonaprisca@gmail.com>
2025-04-12 12:01:08 +0900

    feat: apply header design

--- components/header.tsx
+++ components/header.tsx
@@ -2,25 +2,73 @@
 
 import Link from "next/link";
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const isTransparent = true;
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <header className="font-semibold mt-4 text-white w-[1200px] mx-auto bg-indigo-500 rounded-lg py-4 px-8">
+    <header
+      className={cn(
+        "font-semibold mt-4 text-white w-[1200px] mx-auto rounded-lg py-4 px-8 font-[family-name:var(--font-poppins)]",
+        {
+          "bg-transparent": isTransparent,
+          "bg-indigo-500": !isTransparent,
+        }
+      )}
+    >
       <nav>
-        <div className="flex justify-between items-center">
-          <div>
-            <Link href="/dashboard" className="text-2xl">
-              Persona Chain AI
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className={cn("text-2xl font-medium mr-8", {
+                "text-indigo-500": isTransparent,
+              })}
+            >
+              PersonaChain AI
             </Link>
-          </div>
-          <div className="flex gap-4 items-center">
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/persona">Persona</Link>
-            <Link href="/">Recommendations</Link>
-            <Link href="/">Automation</Link>
-            <Link href="/">Community</Link>
-            <DynamicWidget />
-          </div>
+
+            <div className="flex gap-4 items-center font-light text-gray-800">
+              <Link
+                href="/dashboard"
+                className={cn({
+                  "font-semibold text-indigo-500": pathname === "/dashboard",
+                })}
+              >
+                Dashboard
+              </Link>
+              <Link
+                href="/persona"
+                className={cn({
+                  "font-semibold text-indigo-500": pathname === "/persona",
+                })}
+              >
+                Persona
+              </Link>
+              <Link
+                href="/recommendations"
+                className={cn({
+                  "font-semibold text-indigo-500":
+                    pathname === "/recommendations",
+                })}
+              >
+                Recommendations
+              </Link>
+              <Link
+                href="/automation"
+                className={cn({
+                  "font-semibold text-indigo-500": pathname === "/automation",
+                })}
+              >
+                Automation
+              </Link>
+            </div>
+          </div>{" "}
+          <DynamicWidget innerButtonComponent={<>Log in</>} />
         </div>
       </nav>
     </header>
